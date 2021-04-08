1222455506531fbbfedb11547a33df5cb5d8d1e3
naoakitakado <naoakitakado666@gmail.com>
2021-04-08 10:10:32 +0900

    ogp一覧を変更しました。

--- ogp-ichiran.php
+++ ogp-ichiran.php
@@ -29,7 +29,6 @@ $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':clients_id', $clients_id, PDO::PARAM_INT);
 $status = $stmt->execute();
 
-
 // var_dump($sql1);
 // var_dump($status);
 // var_dump($status1);
@@ -49,24 +48,29 @@ if ($status == false) {
 //   exit;
 }
 
+// $pdo1 = connect_to_db();
 
-$sql1 = "SELECT * ,COUNT(clients_id=:clients_id) AS project_counts FROM ogp_table2 where clients_id =:clients_id";
+// $sql1 = "SELECT * ,COUNT(clients_id=:clients_id) AS project_counts FROM ogp_table2 where clients_id =:clients_id";
 
-$stmt1 = $pdo->prepare($sql1);
-$stmt1->bindValue(':clients_id', $clients_id, PDO::PARAM_INT);
-$status1 = $stmt1->execute();
+// $stmt1 = $pdo1->prepare($sql1);
+// $stmt1->bindValue(':clients_id',$clients_id, PDO::PARAM_INT);
+// $status1 = $stmt1->execute();
 
 
-if ($status1 == false) {
-  // var_dump("hoge");
-  // exit;
-  $error = $stmt->errorInfo();
-  echo json_encode(["error_msg" => "{$error[2]}"]);
-  exit();
-} else {
-  $posts1 = $stmt1->fetch(PDO::FETCH_ASSOC);
-  $project_counts = $posts1["project_counts"];
-}
+// var_dump($status1);
+// exit;
+
+
+// if ($status1 == false) {
+//   // var_dump("hoge");
+//   // exit;
+//   $error = $stmt->errorInfo();
+//   echo json_encode(["error_msg" => "{$error[2]}"]);
+//   exit();
+// } else {
+//   $posts1 = $stmt1->fetch(PDO::FETCH_ASSOC);
+//   $project_counts = $posts1["project_counts"];
+// }
 
 
 ?>
@@ -106,7 +110,7 @@ if ($status1 == false) {
     </header>
     <main>
 <p><span><?= $name ?></span>  様ありがとうございます。<br>
-現在進行中のプロジェクトは<span class="project-kensu"><?= $project_counts ?></span> 件です。</p>
+現在進行中のプロジェクトは<span class="project-kensu"><?= $project_counts ?>0</span> 件です。</p>
 <br>
 
 <?php foreach ($posts as $post) : ?>
