c180b23d43c8765cf55a8b726250530a1d804199
naoakitakado <naoakitakado666@gmail.com>
2021-04-13 13:42:25 +0900

    一部変更分up

--- php_act/functions.php
+++ php_act/functions.php
@@ -1,4 +1,4 @@
-<?php
+<php
 
 function connect_to_db()
 {
@@ -36,20 +36,20 @@ function connect_to_db()
 
 
 
-// function connect_to_db()
-// {
+function connect_to_db()
+ {
 
-//   $dbn = 'mysql:dbname=86f5966d11215db41d6d33f7107303b7;charset=utf8;port=3306;host=mysql-2.mc.lolipop.lan';
-//   $user = '86f5966d11215db41d6d33f7107303b7';
-//   $pwd = 'ROOTroot0627';
+   $dbn = 'mysql:dbname=86f5966d11215db41d6d33f7107303b7;charset=utf8;port=3306;host=mysql-2.mc.lolipop.lan';
+   $user = '86f5966d11215db41d6d33f7107303b7';
+   $pwd = 'ROOTroot0627';
 
-//   try {
-//     return new PDO($dbn, $user, $pwd);
-//   } catch (PDOException $e) {
-//     echo json_encode(["db error" => "{$e->getMessage()}"]);
-//     exit();
-//   }
-// }
+   try {
+     return new PDO($dbn, $user, $pwd);
+   } catch (PDOException $e) {
+     echo json_encode(["db error" => "{$e->getMessage()}"]);
+     exit();
+   }
+ }
 
 
 
