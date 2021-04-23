eb87f6e318482b9489f3cf70bc64c373e9661bb1
naoakitakado <naoakitakado666@gmail.com>
2021-04-23 13:02:15 +0900

    up

--- php_act/ogp_act.php
+++ php_act/ogp_act.php
@@ -18,7 +18,12 @@ $production_period = $_POST["production_period"];
 $remote_availability = $_POST["remote_availability"];
 
 // https://res.cloudinary.com/defgonsxv/image/upload/      v1618795101/banar1_mnwvwv.png
-$v1 = 'https://res.cloudinary.com/defgonsxv/image/upload/l_text:Sawarabi%20Gothic_95_black:';
+// $v1 = 'https://res.cloudinary.com/defgonsxv/image/upload/l_text:Sawarabi%20Gothic_95_black:';
+$v1 = 'https://res.cloudinary.com/defgonsxv/image/upload/l_text:zcekgbuh654xmkklci8z.otf
+
+
+
+_95_black:';
 $img_in1 = $project_title;
 $img_in2 = $job_category;
 $v3 = ',co_rgb:fff,w_1270,c_fit/v1618795101/banar1_mnwvwv.png';
